d57fa70ac1e17c6ccc065671872528ae65c4dbfb
Alessandro <alessandrosilvaprofessor@gmail.com>
2026-01-22 16:05:04 -0300

    Adicionando aula 10 JavaScript

---
+++ aula010/index.js
@@ -0,0 +1,18 @@
+// Escrever um comentário
+// Comentário de linha
+/*
+Comentário
+de
+Bloco
+*/
+console.log('Olá Mundo!'); // Aqui tem um comentário
+console.log('Linha 1');
+// console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// Será ignorado
+/**
+ * Linha 1
+ * Linha 2
+ * Linha 3
+ */
