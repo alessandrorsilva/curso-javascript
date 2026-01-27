092b20f73e9af23698b5041653a3a2dc04320b39
Alessandro <alessandrosilvaprofessor@gmail.com>
2026-01-27 11:53:28 -0300

    Adicionando aulas 21 e 22

---
+++ aula022/js/script.js
@@ -0,0 +1,8 @@
+let num1;
+let num2;
+let resultado;
+num1 = Number(prompt('Digite o primeiro número'));
+num2 = Number(prompt('Digite o segundo número'));
+resultado = num1 + num2;
+alert(`A soma é: ${resultado}`);
+console.log(`${num1} + ${num2} = ${resultado}`);
